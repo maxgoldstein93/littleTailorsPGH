627a15c9d0d1401d241e88c7c476c2402aa3d03a
Max Goldstein <magoldstein93@gmail.com>
2020-12-15 14:33:23 -0500

    created react app

---
+++ littletailors/frontend/src/data.js
@@ -0,0 +1,66 @@
+const data ={
+    products: [
+        {
+            _id: "1",
+            name: "Chicken Kreplach Dumplings",
+            category: "Food",
+            image:"/images/dumpling-1.png",
+            price: 12.99,
+            rating: 5,
+            numReviews: 10,
+            description: "magically delicious"
+        },
+        {
+            _id: "2",
+            name: "Beef Kreplach Dumplings",
+            category: "Food",
+            image:"/images/dumpling-1.png",
+            price: 12.99,
+            rating: 4.5,
+            numReviews: 9,
+            description: "magically delicious"
+        },
+        {
+            _id: "3",
+            name: " Brisket Kreplach Dumplings",
+            category: "Food",
+            image:"/images/dumpling-1.png",
+            price: 12.99,
+            rating: 5,
+            numReviews: 8,
+            description: "magically delicious"
+        },
+        {
+            _id: "4",
+            name: "Chicken Kreplach Dumplings",
+            category: "Food",
+            image:"/images/dumpling-1.png",
+            price: 12.99,
+            rating: 5,
+            numReviews: 7,
+            description: "magically delicious"
+        },
+        {
+            _id: "5",
+            name: "Lobster Kreplach Dumplings",
+            category: "Food",
+            image:"/images/dumpling-1.png",
+            price: 12.99,
+            rating: 2,
+            numReviews: 6,
+            description: "magically delicious"
+        },
+        {
+            _id: "6",
+            name: " Mystery Kreplach Dumplings",
+            category: "Food",
+            image:"/images/dumpling-1.png",
+            price: 11.99,
+            rating: 4,
+            numReviews: 8,
+            description: "magically delicious"
+        }
+        
+    ],
+};
+export default data;
