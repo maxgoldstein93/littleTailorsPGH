0c7ffd321f2280ea0fb892cf9c93cbbb1e9a1c5b
Max Goldstein <magoldstein93@gmail.com>
2020-12-17 12:37:55 -0500

    Loading box and MessageBox

---
+++ littletailors/frontend/src/components/MessageBox.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Messagebox(props){
+    return (
+        <div className={`alert alert-${props.variant || "info"}`}>{props.children}
+          
+
+        </div>
+    );
+}
